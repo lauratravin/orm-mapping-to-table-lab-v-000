ea2b106c76e3976d29f003ed4ac053fde69007c5
Laura Messa Travin <laura.travin@gmail.com>
2018-10-19 17:21:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,8 +26,15 @@ class Student
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
-
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
+
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    song
+  end
+
 
 
 end
