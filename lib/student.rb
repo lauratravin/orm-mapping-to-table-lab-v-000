a7f70f29f79f0b9e10d42502cf51772b4bdac917
Laura Messa Travin <laura.travin@gmail.com>
2018-10-19 17:16:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,7 +8,7 @@ class Student
     @grade = grade
   end
 
-  ef self.create_table
+  def self.create_table
     sql =  <<-SQL
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
